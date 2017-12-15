c46a2fc9ccde369e6122f7a62c76e18ece6c9517
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2017-12-15 10:55:09 +0100

    State composition with Selectors

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -1,14 +1,45 @@
-import * as fromPizzas from './../actions/pizzas.action';
-import { Pizza } from './../../models/pizza.model';
+import * as fromPizzas from "./../actions/pizzas.action";
+import { Pizza } from "./../../models/pizza.model";
 
 export interface PizzaState {
-  data: Pizza[],
-  loaded: boolean,
-  loading: boolean
+  data: Pizza[];
+  loaded: boolean;
+  loading: boolean;
 }
 
 export const initialState = {
-  data: [],
+  data: [
+    {
+      name: "Blazin' Inferno",
+      toppings: [
+        {
+          id: 10,
+          name: "pepperoni"
+        },
+        {
+          id: 9,
+          name: "pepper"
+        },
+        {
+          id: 3,
+          name: "basil"
+        },
+        {
+          id: 4,
+          name: "chili"
+        },
+        {
+          id: 7,
+          name: "olive"
+        },
+        {
+          id: 2,
+          name: "bacon"
+        }
+      ],
+      id: 1
+    }
+  ],
   loaded: false,
   loading: false
 };
@@ -17,9 +48,7 @@ export function reducer(
   state = initialState,
   action: fromPizzas.PizzasAction
 ): PizzaState {
-
-  switch(action.type) {
-
+  switch (action.type) {
     case fromPizzas.LOAD_PIZZAS: {
       return {
         ...state,
@@ -42,8 +71,11 @@ export function reducer(
         loaded: false
       };
     }
-
   }
 
   return state;
 }
+
+export const getPizzasLoading = (state: PizzaState) => state.loading;
+export const getPizzasLoaded = (state: PizzaState) => state.loaded;
+export const getPizzas = (state: PizzaState) => state.data;
