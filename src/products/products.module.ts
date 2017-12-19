76a83e05f72e84840fe0c6d89b3debca1336852f
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2017-12-19 17:51:52 +0100

    Toppings Guard

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,42 +1,42 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
 
-import { reducers, effects } from './store';
+import { reducers, effects } from "./store";
 
 // components
-import * as fromComponents from './components';
+import * as fromComponents from "./components";
 
 // containers
-import * as fromContainers from './containers';
+import * as fromContainers from "./containers";
 
 // guards
-import * as fromGuards from './guards';
+import * as fromGuards from "./guards";
 
 // services
-import * as fromServices from './services';
+import * as fromServices from "./services";
 
 // routes
 export const ROUTES: Routes = [
   {
-    path: '',
+    path: "",
     canActivate: [fromGuards.PizzasGuard],
-    component: fromContainers.ProductsComponent,
+    component: fromContainers.ProductsComponent
   },
   {
-    path: 'new',
-    canActivate: [fromGuards.PizzasGuard],
-    component: fromContainers.ProductItemComponent,
+    path: "new",
+    canActivate: [fromGuards.PizzasGuard, fromGuards.ToppingsGuard],
+    component: fromContainers.ProductItemComponent
   },
   {
-    path: ':pizzaId',
-    canActivate: [fromGuards.PizzaExistsGuards],
-    component: fromContainers.ProductItemComponent,
+    path: ":pizzaId",
+    canActivate: [fromGuards.PizzaExistsGuards, fromGuards.ToppingsGuard],
+    component: fromContainers.ProductItemComponent
   }
 ];
 
@@ -46,11 +46,11 @@ export const ROUTES: Routes = [
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forChild(ROUTES),
-    StoreModule.forFeature('products', reducers),
+    StoreModule.forFeature("products", reducers),
     EffectsModule.forFeature(effects)
   ],
   providers: [...fromServices.services, ...fromGuards.guards],
   declarations: [...fromContainers.containers, ...fromComponents.components],
-  exports: [...fromContainers.containers, ...fromComponents.components],
+  exports: [...fromContainers.containers, ...fromComponents.components]
 })
 export class ProductsModule {}
